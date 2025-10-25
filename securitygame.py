a9bb95842aa7132700364b03d68ecad64d3b0d49
morenatlale10-sketch <morenatlale10@gmail.com>
2025-10-25 08:49:42 +0200

    Add files via upload

---
+++ securitygame.py
@@ -0,0 +1,12 @@
+from main import security
+from argon2 import PasswordHasher
+ph = PasswordHasher()
+import hashlib
+class basic:
+    if PASSWORD123 == "Morena is the best coder":
+        security123 = True
+        a = ph.hash(security123)
+        if a == "$argon2id$v=19$m=65536,t=3,p=4$kZB7rYkzMut5Ko+8CGv4sg$3DBLgSYLkKfwZ3/BhdzI0xEAdMbdK7AQEAZ7npWFWk8":
+            security1234 = "qwertyuiopasdfghjklzxcvbnm"
+        else: security1234 = "incorrect"
+    else: security123 = False
